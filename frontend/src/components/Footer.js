b0c071b2ac1adb11eab0f94699f7cf15301bc405
damrek <dmarecas@gmail.com>
2021-04-01 16:33:40 +0200

    Fix sticky footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core';
 
 const Footer = () => {
   return (
     <footer>
       <Container maxWidth="sm" align="center">
         <Grid item xs={12}>
-          Copyright &copy; Ecommerce
+          <Typography variant="caption">Copyright &copy; Ecommerce</Typography>
         </Grid>
       </Container>
     </footer>
