020fa866bf7a54e48bde78c7464ad475efc6f32d
damrek <dmarecas@gmail.com>
2021-04-02 19:17:47 +0200

    Added create, update and delete product controllers and routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,19 @@
 import express from 'express';
 const router = express.Router();
-import { getProductById, getProducts } from '../controllers/productController.js';
+import {
+  createProduct,
+  deleteProduct,
+  getProductById,
+  getProducts,
+  updateProduct,
+} from '../controllers/productController.js';
+import { admin, protect } from '../middleware/authMiddleware.js';
 
-router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
